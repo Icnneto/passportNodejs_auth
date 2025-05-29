e459d845cec676d332a1789c4c7f244b6dfc208c
Israel Nunes <israelnetonunes@gmail.com>
2025-05-29 19:45:27 -0300

    refact: cors

--- backend/api/app.js
+++ backend/api/app.js
@@ -17,7 +17,9 @@ dotenv.config();
  */
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
